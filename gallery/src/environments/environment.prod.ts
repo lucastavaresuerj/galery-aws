2ac2396f988e2fee07f77ca5608c84047e139eb7
Lucas Tavares <lucastavaresuerj2016@gmail.com>
2022-02-09 09:42:23 -0300

    [build] adding the environments folder but not incluiding the environment.local.ts file

---
+++ gallery/src/environments/environment.prod.ts
@@ -0,0 +1,19 @@
+const { env } = process;
+
+export const environment = {
+  production: true,
+  amplifyConfig: {
+    Auth: {
+      identityPoolId: env['IDENTITY_POOL_ID'], //REQUIRED - Amazon Cognito Identity Pool ID
+      region: env['COGNITO_REGION'], // REQUIRED - Amazon Cognito Region
+      userPoolId: env['USERPOOL_ID'], //OPTIONAL - Amazon Cognito User Pool ID
+      userPoolWebClientId: env['USERPOOL_WEBCLIENT_ID'], //OPTIONAL - Amazon Cognito Web Client ID
+    },
+    Storage: {
+      AWSS3: {
+        bucket: env['S3_STORAGE_NAME'], //REQUIRED -  Amazon S3 bucket name
+        region: env['S3_STORAGE_REGION'], //OPTIONAL -  Amazon service region
+      },
+    },
+  },
+};
