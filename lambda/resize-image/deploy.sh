bfaec66861c3399a5b178326a1303c8bd23710f7
Lucas Tavares <lucastavaresuerj2016@gmail.com>
2022-02-23 09:23:34 -0300

    [lambda] add back slash to deploy.sh

--- lambda/resize-image/deploy.sh
+++ lambda/resize-image/deploy.sh
@@ -1,7 +1,8 @@
-# sam deploy \
-#   --stack-name "${STACK_NAME}-${FUNC_NAME}"
-#   --s3-bucket ${S3_BUCKET_DEPLOY}
-#   --s3-prefix ${FUNC_NAME} 
-#   --parameter-overrides \
-#     S3BucketImage=${S3_BUCKET_IMAGE}
 echo "name: ${STACK_NAME}-${FUNC_NAME}, s3-bucket: ${S3_BUCKET_DEPLOY}, s3-prefix: ${FUNC_NAME}, S3BucketImage: ${S3_BUCKET_IMAGE}"
+
+sam deploy \
+  --stack-name "${STACK_NAME}-${FUNC_NAME}" \
+  --s3-bucket ${S3_BUCKET_DEPLOY} \
+  --s3-prefix "lambda/${FUNC_NAME}" \
+  --parameter-overrides \
+    S3BucketImage=${S3_BUCKET_IMAGE}
--- scripts/deploy-lambdas.sh
+++ scripts/deploy-lambdas.sh
@@ -2,7 +2,6 @@ cd lambda
 
 for folder in *; do
   cd ${folder}
-  sam build
   FUNC_NAME=${folder} ./deploy.sh
   cd ../
 done
