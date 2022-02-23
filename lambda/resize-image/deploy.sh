5e054582d62c0759d4fc530e6477ccd600ba8a48
Lucas Tavares <lucastavaresuerj2016@gmail.com>
2022-02-23 15:42:13 -0300

    [lambda] Adding sns

--- lambda/resize-image/deploy.sh
+++ lambda/resize-image/deploy.sh
@@ -1,8 +1,12 @@
-echo "name: ${STACK_NAME}-${FUNC_NAME}, s3-bucket: ${S3_BUCKET_DEPLOY}, s3-prefix: ${FUNC_NAME}, S3BucketImage: ${S3_BUCKET_IMAGE}"
+echo "name: ${STACK_NAME}-${FUNC_NAME}, \
+      s3-bucket: ${S3_BUCKET_DEPLOY}, \
+      s3-prefix: ${FUNC_NAME}, \
+      S3BucketImage: ${S3_BUCKET_IMAGE}, \
+      S3_BUCKET_IMAGE_SNS: ${S3_BUCKET_IMAGE_SNS}"
 
 sam deploy \
   --stack-name "${STACK_NAME}-${FUNC_NAME}" \
   --s3-bucket ${S3_BUCKET_DEPLOY} \
   --s3-prefix "lambda/${FUNC_NAME}" \
   --parameter-overrides \
-    S3BucketImage=${S3_BUCKET_IMAGE}
+    S3SNSTopic=${S3_BUCKET_IMAGE_SNS}
