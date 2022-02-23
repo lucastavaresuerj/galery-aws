5f9c25812f6ecdce0c33b191cdf95ab0c7a2b017
Lucas Tavares <lucastavaresuerj2016@gmail.com>
2022-02-23 09:19:35 -0300

    [lambda] test env vars

--- lambda/resize-image/deploy.sh
+++ lambda/resize-image/deploy.sh
@@ -1,7 +1,7 @@
-sam deploy \
-  --stack-name "${STACK_NAME}-${folder}"
-  --s3-bucket ${S3_BUCKET_DEPLOY}
-  --s3-prefix ${folder} 
-  --parameter-overrides \
-    S3BucketImage=${S3_BUCKET_IMAGE}
-  
+# sam deploy \
+#   --stack-name "${STACK_NAME}-${FUNC_NAME}"
+#   --s3-bucket ${S3_BUCKET_DEPLOY}
+#   --s3-prefix ${FUNC_NAME} 
+#   --parameter-overrides \
+#     S3BucketImage=${S3_BUCKET_IMAGE}
+echo "name: ${STACK_NAME}-${FUNC_NAME}, s3-bucket: ${S3_BUCKET_DEPLOY}, s3-prefix: ${FUNC_NAME}, S3BucketImage: ${S3_BUCKET_IMAGE}"
