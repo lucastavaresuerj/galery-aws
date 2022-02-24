c1a020f467e2fa86fc8498767554ed69bb3dde73
Lucas Tavares <lucastavaresuerj2016@gmail.com>
2022-02-24 14:48:40 -0300

    [lambda] add role with s3 policies to lambda

--- lambda/resize-image/deploy.sh
+++ lambda/resize-image/deploy.sh
@@ -4,5 +4,6 @@ sam deploy \
   --s3-bucket ${S3_BUCKET_DEPLOY} \
   --s3-prefix "lambda/${FOLDER_NAME}" \
   --parameter-overrides \
-    S3SNSTopic=${S3_BUCKET_IMAGE_SNS} \
+      S3SNSTopic=${S3_BUCKET_IMAGE_SNS} \
+      S3StorageBucketName=${S3_BUCKET_IMAGE} \
   --capabilities CAPABILITY_IAM
